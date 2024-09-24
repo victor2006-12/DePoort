4f22d999e0abcf5e8a0d361ef33bf3c4365629e5
sem <semdronten@gmail.com>
2024-09-24 13:03:16 +0200

    migrations voor de tabbellen in de database

--- database/migrations/2024_09_23_113118_create_admins_table.php
+++ database/migrations/2024_09_23_113118_create_admins_table.php
@@ -12,7 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->id();
+            $table->id('admin_id');
+            $table->binary('foto');
+            $table->string('voornaam', 255);
+            $table->string('tussenvoegsel', 255)->nullable();
+            $table->string('achternaam', 255);
+            $table->string('adres', 255);
+            $table->string('postcode', 6);
+            $table->string('woonplaats', 255);
+            $table->string('land', 255);
+            $table->string('telefoon', 15);
+            $table->string('wachtwoord', 255);         
             $table->timestamps();
         });
     }
