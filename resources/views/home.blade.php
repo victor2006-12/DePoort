4b67507250b48309cc1180fbb0919d406c987723
ErminDeltion <97108201@st.deltion.nl>
2024-10-18 09:17:16 +0200

    navbar text bold als je naar andere pagina gaat

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,8 @@
 </div>
 @endsection
 
+
+
 @section('content')
 <div class="container mt-4 content">
     <!-- Eén grote kalender -->
@@ -29,13 +31,13 @@
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
 <script src='files/index.global.js'></script>
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         var events = @json($events); // Haal de evenementen op van de controller
 
         // Maak een enkele kalender
         var calendarEl = document.getElementById('main-calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
-            plugins: [ FullCalendar.GoogleCalendar.default ],
+            plugins: [FullCalendar.GoogleCalendar.default],
             googleCalendarApiKey: 'AIzaSyDbRbI8q91m56Hg9jycf00a3zDKbqQGne4',
             events: {
                 googleCalendarId: 'cd099f93a95fe6e661355943e2f1f1637b8cd9567e23927a004421207f3a4d27@group.calendar.google.com'
@@ -46,7 +48,7 @@
             slotDuration: '01:00:00',
             slotLabelInterval: '01:00',
             locale: 'nl', // Gebruik Nederlandse vertaling
-            eventClick: function(info) {
+            eventClick: function (info) {
                 alert('Afspraak: ' + info.event.title + '\nStart: ' + info.event.start + '\nEindigt: ' + info.event.end);
             }
         });
