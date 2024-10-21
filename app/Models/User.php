88aa4a2f566486c3083b96d064c2889ac481871b
ErminDeltion <97108201@st.deltion.nl>
2024-10-21 10:40:24 +0200

    has role verwijdert

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable ;
 
     /**
      * Aangepaste primaire sleutel.
