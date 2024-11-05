2b530a8db099be4f1640846151d30a7c1b326542
sem <semdronten@gmail.com>
2024-11-05 13:54:22 +0100

    if statement weggehaald

--- resources/views/Dokter/dokter.blade.php
+++ resources/views/Dokter/dokter.blade.php
@@ -10,10 +10,7 @@
 @section('content')
 <div class="clients-container">
     <h3>Cliënten</h3>
-    <a href="/dokter/meldingen">Meldingen</a>
-    @if($)
-        <h3>Dokter rol</h3>
-    @endif
+    <a href="/dokter/meldingen">Meldingen</a>   
     <table class="clients-table">
         <thead>
             <tr>
