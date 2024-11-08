19979a6f9a30ac8b73d635bb9dc94ad26b59b39b
ErminDeltion <97108201@st.deltion.nl>
2024-11-08 10:49:54 +0100

    a

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -11,7 +11,7 @@ class Client extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'id',
+        'gebruikers_id',
         'foto',
         'voornaam',
         'tussenvoegsel',
--- resources/views/Afspraak.blade.php
+++ resources/views/Afspraak.blade.php
@@ -19,7 +19,7 @@
         @csrf
 
         <div class="mb-3">
-            <label for="gebruikers_id" class="form-label">Gebruikers ID (voor nu invullen met 1)</label>
+            <label for="gebruikers_id" class="form-label">ID (voor nu invullen met 1)</label>
             <input type="number" class="form-control" id="gebruikers_id" name="gebruikers_id" value="1" required>
         </div>
         <div class="mb-3">
