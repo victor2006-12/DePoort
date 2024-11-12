b9911a3e819ce321148037dbeab2540a8ac6cfdc
ErminDeltion <97108201@st.deltion.nl>
2024-11-12 12:47:15 +0100

    w

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -20,7 +20,7 @@
             aria-expanded="false" style="text-decoration: none; color: black;">
             <img src="{{ asset('image/client-photo.jpg') }}" alt="Client Photo" class="rounded-circle"
                 style="width: 40px; height: 40px;">
-            <span class="ms-2">{{ $userName ?? 'nahh' }}</span>
+            <span class="ms-2">{{ $userName ?? 'niks' }}</span>
         </a>
 
         <!-- Dropdown Menu -->
